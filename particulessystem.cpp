f8742c9799f12064bafd9fafd5030886e1266525
dmaronnier <dimitri.maronnier@etu.montpellier.fr>
2017-10-13 12:50:14 +0200

    initial commit

---
+++ particulessystem.cpp
@@ -0,0 +1,7 @@
+#include "particulessystem.h"
+
+ParticulesSystem::ParticulesSystem()
+{
+
+}
+
---
+++ particulessystem.h
@@ -0,0 +1,11 @@
+#ifndef PARTICULESSYSTEM_H
+#define PARTICULESSYSTEM_H
+
+
+class ParticulesSystem
+{
+public:
+    ParticulesSystem();
+};
+
+#endif // PARTICULESSYSTEM_H
